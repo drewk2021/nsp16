94a84c5a895fa18b9e437d21c1d32a30a63a057d
unknown <drew.kessler.21@bishops.com>
2020-11-12 10:40:17 -0800

    convert csv into pd.DataFrame()

--- CODE/retrieveDescriptors.py
+++ CODE/retrieveDescriptors.py
@@ -7,6 +7,11 @@ def getDescriptors(path):
     """
     Read in CSV to pandas Data Frame Object.
     """
+    descriptors = pd.read_csv(path, header = 0, index_col = 0)
 
-    return
+    return descriptors
 
+if __name__ == '__main__':
+    nsp16drugsdescriptors = getDescriptors("C:\\Users\\Tamara\\Desktop\\nsp16\\descriptors\\nsp16drugsdescriptors.csv")
+    print(nsp16drugsdescriptors.dtypes)
+    print(nsp16drugsdescriptors.head())
