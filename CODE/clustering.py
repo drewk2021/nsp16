91e42c7143887d3084b8512268ffd9ef4e38a876
unknown <drew.kessler.21@bishops.com>
2020-11-12 18:39:24 -0800

    cleaned NA values in data frames, KMeans functionality

--- CODE/clustering.py
+++ CODE/clustering.py
@@ -1,12 +1,13 @@
 import pandas as pd
 from sklearn.cluster import KMeans
-from retrieveDescriptors import getDescriptors
+from retrieveDescriptors import getDescriptors, clean_dataset
 
 
 drugs = getDescriptors("C:\\Users\\Tamara\\Desktop\\nsp16\\descriptors\\nsp16drugsdescriptors.csv")
 controls = getDescriptors("C:\\Users\\Tamara\\Desktop\\nsp16\\descriptors\\randomcontrolsdescriptors.csv")
 
 unsupervisedComb = pd.concat([drugs, controls])
+print(unsupervisedComb.shape)
 
-kmeans = KMeans(n_clusters = 6, random_state = 0).fit(unsupervisedComb)
-print(kmeans.labels_)
+kmeans = KMeans(n_clusters = 5, random_state = 0).fit_predict(unsupervisedComb,y = None, sample_weight = None)
+print(kmeans)
--- CODE/retrieveDescriptors.py
+++ CODE/retrieveDescriptors.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import numpy as np
 
 
 
@@ -8,10 +8,26 @@ def getDescriptors(path):
     Read in CSV to pandas Data Frame Object.
     """
     descriptors = pd.read_csv(path, header = 0, index_col = 0)
-
+    # descriptors.fillna(0,inplace=True) substituting NA values for 0
+    descriptors = descriptors.apply(lambda x: x.fillna(x.mean()),axis=0) # substituting NA values for columnwise means
     return descriptors
+
+
+
+def clean_dataset(df):
+    assert isinstance(df, pd.DataFrame), "df needs to be a pd.DataFrame"
+    df.dropna(inplace=True)
+    indices_to_keep = ~df.isin([np.nan, np.inf, -np.inf]).any(1)
+    return df[indices_to_keep].astype(np.float64)
+
+
+
+def check_dataset(df):
+    return
+
+
 
 if __name__ == '__main__':
     nsp16drugsdescriptors = getDescriptors("C:\\Users\\Tamara\\Desktop\\nsp16\\descriptors\\nsp16drugsdescriptors.csv")
-    print(nsp16drugsdescriptors.dtypes)
-    print(nsp16drugsdescriptors.head())
+    print(nsp16drugsdescriptors.shape)
+    print(clean_dataset(nsp16drugsdescriptors).head(10))
